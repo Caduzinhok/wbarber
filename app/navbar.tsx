ebf73a0ad459eade6652829363f2a0c1d84e9cdb
Caduzinhok <carlos_fire1@outlook.com>
2024-09-22 19:13:32 -0300

    add menu mobile

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,23 +1,31 @@
+import MenuNavbar, { menuObj } from "@/components/menu";
+import { useEffect } from "react";
+import { useState } from "react";
+
 export default function Navbar() {
+    const [width, setWidth] = useState<number | undefined>(undefined)
+
+    useEffect(() => {
+        if(typeof window === undefined){
+            return
+        }
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return (
         <div className="w-full h-24 bg-[#0D121E] md:px-32 p-4 flex items-center justify-between">
             <img src="./logo.png" alt="Logo w-barber" className="md:size-16 size-12" />
-            <ul className="hidden md:flex md:space-x-10 space-x text-white list-none text-xs">
-                <li>
-                    <a href="#Sobre" className="text-base font-normal">Sobre</a>
-                </li>
-                <li>
-                    <a href="#Nossos_servicos" className="text-base font-normal">Nossos Serviços</a>
-                </li>
-                <li>
-                    <a href="#Depoimentos" className="text-base font-normal">Depoimentos</a>
-                </li>
-                <li>
-                    <a href="#" className="text-base font-normal">Equipe</a>
-                </li>
-                <li>
-                    <a href="#Contato" className="text-base font-normal">Entre em Contato</a>
-                </li>
+            <ul className="hidden lg:flex md:space-x-10 space-x text-white list-none text-xs">
+                {menuObj.map((item) => (
+                    <li key={item.name}>
+                        <a href={item.href} className="text-base font-normal">{item.name}</a>
+                    </li>
+                ))}
             </ul>
             <div className="flex md:space-x-10 space-x-4 items-center">
                 <a href="https://www.instagram.com/wbarberclub/">
@@ -28,6 +36,9 @@ export default function Navbar() {
                         Agendar Agora
                     </a>
                 </button>
+                {width && width < 1024 &&
+                    <MenuNavbar />
+                }
             </div>
         </div>
     )
---
+++ components/menu.tsx
@@ -0,0 +1,38 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { div } from 'framer-motion/client'
+import { MenuIcon } from 'lucide-react'
+
+
+export const menuObj = [
+  { name: 'Sobre', href: '#Sobre' },
+  { name: 'Nossos Serviços', href: '#Nossos_servicos' },
+  { name: 'Depoimentos', href: '#Depoimentos' },
+  { name: 'Equipe', href: '#' },
+  { name: 'Entre em Contato', href: '#Contato' },
+]
+
+export default function MenuNavbar() {
+  return (
+      <Menu>
+        <MenuButton className="inline-flex items-center gap-2 py-1.5 px-3 font-semibold focus:outline-none">
+          <MenuIcon className='size-10 text-white' />
+        </MenuButton>
+        <MenuItems
+          transition
+          anchor="bottom end"
+          className="max-w-max origin-top-right rounded-xl border border-white/5 bg-white/5 p-1 text-sm/6 text-white transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
+        >
+          <div className='flex flex-col p-4'>
+          {menuObj.map((item) => (
+              <MenuItem key={item.name}>
+                <a href={item.href} className=' text-white'>
+                  {item.name}
+                </a>
+              </MenuItem>
+            ))}
+          </div>
+            
+        </MenuItems>
+      </Menu>
+  )
+}
